b42833d2fbe3bda75f17d3ddf295f13ac8fb56e7
Aditya <adityagaikwad693@gmail.com>
2022-09-14 15:20:46 +1000

    controllers fixed

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -4,12 +4,11 @@ module.exports ={
     getUsers(req,res){
         User.find()
         .then((user)=> res.json(user))
-        console.log("This work")
         .catch((err) => res.status(500).json(err));
     },
     createUser(req,res){
         User.create(req.body)
-        .thrn((dbUserData) => res.json(dbUserData))
+        .then((dbUserData) => res.json(dbUserData))
         .catch((err) => res.status(500).json(err));
     }
 }
