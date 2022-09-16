0cafec5afb94a7f8b018586bbe8164a75e448fc4
Aditya <adityagaikwad693@gmail.com>
2022-09-16 16:59:54 +1000

    fixed reaction

--- controllers/reactionControllers.js
+++ controllers/reactionControllers.js
@@ -8,6 +8,12 @@ module.exports = {
             { $addToSet: { reactions: req.body } },
             { runValidators: true, new: true }
         )
+        .then((dbReactionData)=>{
+            if(!dbReactionData){
+                return res.status(404).json({message: 'Can not find user, Thought as been made'})
+            }
+            res.status(200).json({message: 'Your reaction as been made'})
+        })
         .catch((err) => res.status(500).json(err));
     }
 }
--- routes/api/reactionRoutes.js
+++ routes/api/reactionRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const {createReaction} = require('../../controllers/reactionControllers');
 
-router.route('/:thoughtId').post(createReaction);
+router.route("/:thoughtId/reactions").post(createReaction);
 
 module.exports = router;
